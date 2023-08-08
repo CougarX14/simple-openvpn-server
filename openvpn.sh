843f3bd70155f1cea62ebf06ffcfe416617111d4
CougarX14 <86085268+CougarX14@users.noreply.github.com>
2023-08-08 08:32:56 -0700

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -70,7 +70,6 @@ echo "NETWORK         = ${NETWORK}"
 
 [ "${ADMINPASSWORD}" == "secret" ] && echo "fatal: password is not set" && exit 1
 
-exit 1
 # Detect Debian users running the script with "sh" instead of bash
 if readlink /proc/$$/exe | grep -qs "dash"; then
 	echo "This script needs to be run with bash, not sh"
